9efb803e4a953c30d9899729f623e39a68629910
Will Dady <willdady@gmail.com>
2025-12-14 16:17:42 +1100

    feat(ui): added global command pallete

--- apps/frontend/app/[orgId]/workspace/[workspaceId]/layout.tsx
+++ apps/frontend/app/[orgId]/workspace/[workspaceId]/layout.tsx
@@ -1,11 +1,14 @@
 import { notFound } from "next/navigation";
 import { ModeToggle } from "@/components/mode-toggle";
+import { CommandMenu } from "@/components/command-menu";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { Kbd } from "@/components/ui/kbd";
+import { Search } from "lucide-react";
 
 export default async function WorkspaceLayout({
   children,
@@ -33,11 +36,17 @@ export default async function WorkspaceLayout({
       <SidebarInset>
         <header className="flex justify-between p-2">
           <SidebarTrigger className="cursor-pointer" />
-          <ModeToggle />
+          <div className="flex items-center gap-4">
+            <Kbd className="text-sm font-sans">
+              <Search className="size-4" /> ⌘K
+            </Kbd>
+            <ModeToggle />
+          </div>
         </header>
         <div className="h-[calc(100vh-2.75rem)] overflow-y-auto">
           {children}
         </div>
+        <CommandMenu orgId={orgId} workspaceId={workspaceId} />
       </SidebarInset>
     </SidebarProvider>
   );
