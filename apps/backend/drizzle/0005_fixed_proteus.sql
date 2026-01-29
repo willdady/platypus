98ca6888efc3b23a3d36265103c0b9b05ee257c2
Will Dady <willdady@gmail.com>
2026-01-29 15:53:53 +1100

    fix(backend): missing migration files

---
+++ apps/backend/drizzle/0005_fixed_proteus.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "account" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "banned" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "ban_reason" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "ban_expires" timestamp;
